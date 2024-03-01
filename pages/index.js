4d671bab0ace663cb2463bbe21a424cc31c8f5a7
Maicon Lima <mai.ky.3000@gmail.com>
2024-03-01 19:26:54 -0300

    style: add `prettier`, `lint:check` and `lint:fix` as scripts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Página inicial</div>
-  )
-}
+  return <div>Página inicial</div>;
+};
 
-export default Home
+export default Home;
